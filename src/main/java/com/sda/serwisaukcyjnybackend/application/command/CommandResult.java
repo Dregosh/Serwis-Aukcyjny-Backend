59369dbbf6006435d12034170a3176eb15a3ff77
adam.zmijewski <adam.zmijewski@bluesoft.com>
2020-12-08 22:15:22 +0100

    Make Command Result static method public

--- src/main/java/com/sda/serwisaukcyjnybackend/application/command/CommandResult.java
+++ src/main/java/com/sda/serwisaukcyjnybackend/application/command/CommandResult.java
@@ -8,27 +8,27 @@ public class CommandResult<T> {
     private final CommandResultType resultType;
     private final T payload;
 
-    private static CommandResult<Void> created() {
+    public static CommandResult<Void> created() {
         return new CommandResult<>(CommandResultType.CREATED, null);
     }
 
-    private static <T> CommandResult<T> created(T payload) {
+    public static <T> CommandResult<T> created(T payload) {
         return new CommandResult<>(CommandResultType.CREATED, payload);
     }
 
-    private static CommandResult<Void> accepted() {
+    public static CommandResult<Void> accepted() {
         return new CommandResult<>(CommandResultType.ACCEPTED, null);
     }
 
-    private static <T> CommandResult<T> accepted(T payload) {
+    public static <T> CommandResult<T> accepted(T payload) {
         return new CommandResult<>(CommandResultType.ACCEPTED, payload);
     }
 
-    private static CommandResult<Void> failed() {
+    public static CommandResult<Void> failed() {
         return new CommandResult<>(CommandResultType.FAILED, null);
     }
 
-    private static <T> CommandResult<T> failed(T payload) {
+    public static <T> CommandResult<T> failed(T payload) {
         return new CommandResult<>(CommandResultType.FAILED, payload);
     }
 }
