b99f1cb36fcb1177d2d60238889722f327a60c88
adam.zmijewski <adam.zmijewski@bluesoft.com>
2020-12-08 21:51:12 +0100

    Command handler

---
+++ src/main/java/com/sda/serwisaukcyjnybackend/application/command/Command.java
@@ -0,0 +1,4 @@
+package com.sda.serwisaukcyjnybackend.application.command;
+
+public interface Command {
+}
---
+++ src/main/java/com/sda/serwisaukcyjnybackend/application/command/CommandDispatcher.java
@@ -0,0 +1,27 @@
+package com.sda.serwisaukcyjnybackend.application.command;
+
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class CommandDispatcher {
+    private final Map<Class<Command>, CommandHandler> handlersMap = new HashMap<>();
+
+    public <R> CommandResult<R> handle(Command command) {
+        if (handlersMap.containsKey(command.getClass())) {
+            return handlersMap.get(command.getClass()).handle(command);
+        }
+        throw new IllegalArgumentException(String.format("Could not find handler for command %s", command.getClass()));
+    }
+
+    public CommandDispatcher(CommandHandler ... handlers) {
+        for (CommandHandler handler : handlers) {
+            if(handlersMap.containsKey(handler.commandClass())) {
+                throw new IllegalArgumentException(String.format("Could not register commands with same commandClass %s", handler.commandClass()));
+            }
+            handlersMap.put(handler.commandClass(), handler);
+        }
+    }
+}
