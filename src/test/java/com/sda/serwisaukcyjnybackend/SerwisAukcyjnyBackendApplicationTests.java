e7a598d4b0278acd0a46fef61dbbda4ba0997dc2
adam.zmijewski <adam.zmijewski@bluesoft.com>
2020-12-12 11:39:11 +0100

    Clear contextLoad no need for pipeline

--- src/test/java/com/sda/serwisaukcyjnybackend/SerwisAukcyjnyBackendApplicationTests.java
+++ src/test/java/com/sda/serwisaukcyjnybackend/SerwisAukcyjnyBackendApplicationTests.java
@@ -1,15 +1,7 @@
 package com.sda.serwisaukcyjnybackend;
 
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SerwisAukcyjnyBackendApplicationTests {
-
-    @Test
-    @Disabled
-    void contextLoads() {
-    }
-
 }
