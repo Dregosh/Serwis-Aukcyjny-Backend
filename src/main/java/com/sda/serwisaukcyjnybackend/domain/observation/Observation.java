f44223e3bc6f6c296d33cb6554587a2b9755c633
Dregosh <matello@upcpoczta.pl>
2020-12-12 22:20:25 +0100

    feature/SAB-6: add Observation entity

---
+++ src/main/java/com/sda/serwisaukcyjnybackend/domain/observation/Observation.java
@@ -0,0 +1,26 @@
+package com.sda.serwisaukcyjnybackend.domain.observation;
+
+import com.sda.serwisaukcyjnybackend.domain.auction.Auction;
+import com.sda.serwisaukcyjnybackend.domain.user.User;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@Data
+@Entity
+public class Observation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "auction_id")
+    private Auction auction;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
