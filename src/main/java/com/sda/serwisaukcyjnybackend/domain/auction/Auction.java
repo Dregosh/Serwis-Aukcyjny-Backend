182fe6dba978d8d686927ae7287306e9ac2f4bf1
Rafal Piekos <rafal.peikos@gmail.com>
2020-12-12 12:47:33 +0100

    impl comments

--- src/main/java/com/sda/serwisaukcyjnybackend/domain/auction/Auction.java
+++ src/main/java/com/sda/serwisaukcyjnybackend/domain/auction/Auction.java
@@ -3,14 +3,15 @@ package com.sda.serwisaukcyjnybackend.domain.auction;
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
-@Getter
-@Setter
+@Data
 @Builder
 public class Auction {
 
@@ -23,13 +24,36 @@ public class Auction {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
 
+    @Version
+    Long version;
+
+    @NotNull
     String title;
+
+    @NotNull
     String description;
+
+    @NotNull
+    @Min(0)
     BigDecimal minPrice;
+
+    @NotNull
+    @Min(0)
     BigDecimal buyNowPrice;
+
+    @NotNull
     Boolean isPromoted;
+
+    @NotNull
     String location;
+
+    @NotNull
     LocalDateTime startDateTime;
+
+    @NotNull
     LocalDateTime endDateTime;
+
+    @NotNull
+    Status status;
 
 }
--- src/main/resources/db/migration/V3__auction.sql
+++ src/main/resources/db/migration/V3__auction.sql
@@ -1,12 +1,13 @@
 CREATE TABLE auction (
     id BIGINT PRIMARY KEY,
+    version BIGINT,
     title varchar(255),
     description text,
     min_price decimal,
     buy_now_price decimal,
-    is_promoted boolean,
-    location varchar(255),
+    is_promoted boolean default 0,
+    location varchar(500),
     start_date_time datetime,
     end_date_time datetime,
-    viewed_count int
+    status enum('CREATED','STARTED','ENDED')
 );
