639283eb53bfe97ebb345a7848a82bb10e82f60a
Dregosh <matello@upcpoczta.pl>
2020-12-13 14:54:53 +0100

    feature/SAB-14

--- src/test/java/com/sda/serwisaukcyjnybackend/view/dashboard/DashboardServiceTest.java
+++ src/test/java/com/sda/serwisaukcyjnybackend/view/dashboard/DashboardServiceTest.java
@@ -3,6 +3,7 @@ package com.sda.serwisaukcyjnybackend.view.dashboard;
 import com.sda.serwisaukcyjnybackend.domain.auction.Auction;
 import com.sda.serwisaukcyjnybackend.domain.auction.AuctionRepository;
 import com.sda.serwisaukcyjnybackend.domain.auction.AuctionStatus;
+import com.sda.serwisaukcyjnybackend.domain.shared.Address;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -11,8 +12,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-
-import static org.mockito.Mockito.when;
+import java.util.ArrayList;
+import java.util.List;
 
 @ExtendWith(MockitoExtension.class)
 class DashboardServiceTest {
@@ -23,24 +24,57 @@ class DashboardServiceTest {
     @InjectMocks
     DashboardService dashboardService;
 
-//    @BeforeAll
+    List<Auction> auctionList = new ArrayList<>();
+
     void prepareAllAuctionsBase() {
+        Auction auction1 = new Auction();
+        auction1.setId(1L);
+        auction1.setVersion(null);
+        auction1.setTitle("komp");
+        auction1.setDescription("");
+        auction1.setMinPrice(BigDecimal.valueOf(100.0));
+        auction1.setBuyNowPrice(BigDecimal.valueOf(200.0));
+        auction1.setIsPromoted(false);
+        auction1.setLocation(
+                new Address("Wawa", "mazowieckie", "lotna", "27", "05-500 "));
+        auction1.setStartDateTime(LocalDateTime.of(2020, 12, 13, 11, 49));
+        auction1.setEndDateTime(LocalDateTime.of(2020, 12, 20, 11, 49));
+        auction1.setStatus(AuctionStatus.STARTED);
+        auction1.setSeller(null);
 
-        /*Auction auction = new Auction();
-        auction.setId(1L);
-        auction.setVersion(null);
-        auction.setTitle("komp");
-        auction.setDescription("");
-        auction.setMinPrice(BigDecimal.valueOf(100.0));
-        auction.setBuyNowPrice(BigDecimal.valueOf(200.0));
-        auction.setIsPromoted(false);
+        Auction auction2 = new Auction();
+        auction2.setId(2L);
+        auction2.setVersion(null);
+        auction2.setTitle("tv");
+        auction2.setDescription("");
+        auction2.setMinPrice(BigDecimal.valueOf(200.0));
+        auction2.setBuyNowPrice(BigDecimal.valueOf(500.0));
+        auction2.setIsPromoted(false);
+        auction2.setLocation(
+                new Address("Wawa", "mazowieckie", "lotna", "27", "05-500"));
+        auction2.setStartDateTime(LocalDateTime.of(2020, 12, 16, 11, 49));
+        auction2.setEndDateTime(LocalDateTime.of(2020, 12, 18, 11, 49));
+        auction2.setStatus(AuctionStatus.STARTED);
+        auction2.setSeller(null);
 
-        auction.setLocation();  // ??
+        Auction auction3 = new Auction();
+        auction3.setId(3L);
+        auction3.setVersion(null);
+        auction3.setTitle("radio");
+        auction3.setDescription("");
+        auction3.setMinPrice(BigDecimal.valueOf(50.0));
+        auction3.setBuyNowPrice(BigDecimal.valueOf(100.0));
+        auction3.setIsPromoted(false);
+        auction3.setLocation(
+                new Address("Wawa", "mazowieckie", "lotna", "27", "05-500"));
+        auction3.setStartDateTime(LocalDateTime.of(2020, 12, 10, 11, 49));
+        auction3.setEndDateTime(LocalDateTime.of(2020, 12, 12, 11, 49));
+        auction3.setStatus(AuctionStatus.ENDED);
+        auction3.setSeller(null);
 
-        auction.setStartDateTime(LocalDateTime.of(2020, 12, 13, 11, 49));
-        auction.setEndDateTime(LocalDateTime.of(2020, 12, 20, 11, 49));
-        auction.setStatus(AuctionStatus.STARTED);
-        auction.setSeller(null);*/
+        auctionList.add(auction1);
+        auctionList.add(auction2);
+        auctionList.add(auction3);
     }
 
 }
