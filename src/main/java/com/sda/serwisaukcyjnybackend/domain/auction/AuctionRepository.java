ef103485b44c10ca1e904d237353ebd1933ce758
adam.zmijewski <adam.zmijewski@bluesoft.com>
2020-12-17 21:37:27 +0100

    Auction filtration

--- src/main/java/com/sda/serwisaukcyjnybackend/domain/auction/AuctionRepository.java
+++ src/main/java/com/sda/serwisaukcyjnybackend/domain/auction/AuctionRepository.java
@@ -2,12 +2,13 @@ package com.sda.serwisaukcyjnybackend.domain.auction;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface AuctionRepository extends JpaRepository<Auction, Long> {
+public interface AuctionRepository extends JpaRepository<Auction, Long>, JpaSpecificationExecutor<Auction> {
 
     List<Auction> findAllBySeller_IdOrderByEndDateTimeAsc(
             Long sellerId, Pageable pageable);
